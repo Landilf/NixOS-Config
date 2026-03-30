a45da9b9931c7307b962854b962c267625f9459d
Landilf <vladrumin4@gmail.com>
2026-03-30 03:46:18 +0300

    update flock and fix power menu

--- .config/RofiScripts/powermenu/powermenu.sh
+++ .config/RofiScripts/powermenu/powermenu.sh
@@ -4,21 +4,18 @@
 : "${LANG:=C.UTF-8}"
 export LC_ALL LANG
 
-back_label="← Back"
-
 	chosen=$(
 		printf "%s\n" \
-			"$back_label" \
 		"󰐥" \
 		"󰜉" \
 		"󰒲" \
 		"󰌾" \
 		"󰀄" |
-		rofi -dmenu -i -selected-row 1 -config "$HOME/.config/RofiScripts/powermenu/P.rasi" -kb-move-char-back "" -kb-move-char-forward "" -kb-custom-1 "Left" -kb-accept-entry "Control+j,Control+m,Return,KP_Enter,Right"
+		rofi -dmenu -i -selected-row 0 -config "$HOME/.config/RofiScripts/powermenu/P.rasi" -kb-move-char-back "" -kb-move-char-forward "" -kb-row-left "Left" -kb-row-right "Right" -kb-accept-entry "Control+j,Control+m,Return,KP_Enter"
 	)
 	rc=$?
 
-if [ "$rc" -eq 10 ] || [ "$chosen" = "$back_label" ]; then
+if [ "$rc" -ne 0 ] || [ -z "$chosen" ]; then
 	exit 0
 fi
 
